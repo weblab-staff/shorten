979cd3fa132a618aa133d5c7686c392bfc73a855
Cory Lynch <cjl2625@gmail.com>
2019-01-07 00:42:28 -0500

    Replace tabs with spaces.

--- app.py
+++ app.py
@@ -7,40 +7,40 @@ app = Flask(__name__)
 conn = sqlite3.connect('urls.db')
 c = conn.cursor()
 try:
-	c.execute('SELECT * FROM urls')
+    c.execute('SELECT * FROM urls')
 except sqlite3.OperationalError as e:
-	print('Creating new table')
-	c.execute('CREATE TABLE urls(short TEXT UNIQUE, full TEXT)')
-	conn.commit()
+    print('Creating new table')
+    c.execute('CREATE TABLE urls(short TEXT UNIQUE, full TEXT)')
+    conn.commit()
 conn.close()
 
 @app.route("/")
 def hello():
-	with sqlite3.connect('urls.db') as conn:
-		c = conn.cursor()
-		c.execute("SELECT * FROM urls")
-		return render_template('index.html', urls=c.fetchall())
+    with sqlite3.connect('urls.db') as conn:
+        c = conn.cursor()
+        c.execute("SELECT * FROM urls")
+        return render_template('index.html', urls=c.fetchall())
 
 @app.route("/<short>")
 def go(short):
-	with sqlite3.connect('urls.db') as conn:
-		c = conn.cursor()
-		c.execute("SELECT full FROM urls WHERE short=?", [short])
-		full = c.fetchone()
-		if full:
-			return redirect(full[0])
-	abort(404)
+    with sqlite3.connect('urls.db') as conn:
+        c = conn.cursor()
+        c.execute("SELECT full FROM urls WHERE short=?", [short])
+        full = c.fetchone()
+        if full:
+            return redirect(full[0])
+    abort(404)
 
 @app.route("/add", methods=['POST'])
 def add():
-	full = request.form['full']
-	short = request.form['short']
-	with sqlite3.connect('urls.db') as conn:
-		c = conn.cursor()
-		c.execute("INSERT OR REPLACE into URLS values (?,?)", [short, full])
-		conn.commit()
+    full = request.form['full']
+    short = request.form['short']
+    with sqlite3.connect('urls.db') as conn:
+        c = conn.cursor()
+        c.execute("INSERT OR REPLACE INTO urls VALUES (?,?)", [short, full])
+        conn.commit()
 
-	return redirect("/")
+    return redirect("/")
 
 http_server = WSGIServer(('', 8000), app)
 http_server.serve_forever()
